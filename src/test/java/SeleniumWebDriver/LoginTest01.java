1181ba3f3e98efe96ff254cf7a7fe94410303800
SaiKeerthi3021 <keerthi3265@gmail.com>
2025-03-15 20:29:46 +0530

    LoginTest01

--- src/test/java/SeleniumWebDriver/LoginTest01.java
+++ src/test/java/SeleniumWebDriver/LoginTest01.java
@@ -1,9 +1,36 @@
 package SeleniumWebDriver;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class LoginTest01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	    WebDriver driver=new ChromeDriver();
+	    
+	    driver.get("http://in-pnq-coe31:5180/pc/PolicyCenter.do");
+	    
+	    driver.findElement(By.name("Login-LoginScreen-LoginDV-username")).sendKeys("su");
+	    
+	    driver.findElement(By.name("Login-LoginScreen-LoginDV-password")).sendKeys("gw");
+	    
+	    driver.findElement(By.xpath("//div[@class='gw-action--inner gw-hasDivider']")).click();
+	    
+	    
+	    WebElement pagelogo = driver.findElement(By.xpath("//div[@id='gw-customer--logo']"));
+	    
+	    boolean status = pagelogo.isDisplayed();
+	    
+	    
+	    System.out.println("Logo of the page is visible" +status);
+	    
+	    
+	    
+	    
 
 	}
 
